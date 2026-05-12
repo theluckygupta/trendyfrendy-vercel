45acfc566090ca3cbde57cc87b4b48b5dad2cdea
Lucky Gupta <44520571+theluckygupta@users.noreply.github.com>
2026-05-12 16:18:12 +0000

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TrendyFrenzy,
+  title: "TrendyFrenzy",
   description: "Luxury Fashion Store by TrendyFrendy",
 };
 
